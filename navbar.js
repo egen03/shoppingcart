f0a74df4c5704c836af447703e8a2a0afec73b83
Student_GA111 <egen03@gmail.com>
2020-07-11 17:00:51 -0400

    created a navbar

---
+++ navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-dom';
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div className="navbar">
+                <Link to="/">Shopping</Link>
+
+                <ul>
+                    <li><Link to="/">Home</Link> </li>
+                    <li><Link to="/">Subscribe</Link> </li>
+                    <li><Link to="/">Cart</Link> </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
