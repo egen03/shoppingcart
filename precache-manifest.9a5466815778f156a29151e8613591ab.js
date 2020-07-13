530ff2b573ef59afd6ca3c6fdc64e6a1c2abed56
Student_GA111 <egen03@gmail.com>
2020-07-13 12:17:58 -0400

    Updates

---
+++ precache-manifest.9a5466815778f156a29151e8613591ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717fda6b4970d55a191844ae0a6a2662",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "bd9cb2fd24930c68b9bc",
+    "url": "/shoppingcart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5430ff41867b8347a64d",
+    "url": "/shoppingcart/static/js/2.329c96b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppingcart/static/js/2.329c96b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9cb2fd24930c68b9bc",
+    "url": "/shoppingcart/static/js/main.4dfffa90.chunk.js"
+  },
+  {
+    "revision": "56156e7d808009d35481",
+    "url": "/shoppingcart/static/js/runtime-main.c0a5f944.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppingcart/static/media/logo.5d5d9eef.svg"
+  }
+]);
